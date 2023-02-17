e35b46897e5832dbd22bd5bc649d2a6c37cf0b5c
Kioivan <ivan.sanz09@gmail.com>
2023-02-17 18:39:59 -0300

    en progreso

---
+++ 00-Rick_and_Morty/src/components/About/About.jsx
@@ -0,0 +1,9 @@
+export default function About() {
+  return (
+    <div style={{ color: "red", marginTop: "150px" }}>
+      <h1>Ivan Sanz - Cohorte 11b</h1>
+      <h3>Mi App de Rick y Morty (M2 - Henry)</h3>
+      <p>Creamos una Wiki de personajes de Rick y Morty, con las caracteristicas principales de cada personaje</p>
+    </div>
+  );
+}
---
+++ 00-Rick_and_Morty/src/components/Error/Error.jsx
@@ -0,0 +1,3 @@
+export default function Error() {
+  return <h1 style={{ color: "red", marginTop: "150px" }}>ERROR 404</h1>;
+}
